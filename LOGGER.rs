1de1ee894d6ebe56d994735135017c5ca7572b5c
JikoUnderscore <59426055+JikoUnderscore@users.noreply.github.com>
2021-12-09 23:04:54 +0200

    Create LOGGER.rs

---
+++ LOGGER.rs
@@ -0,0 +1,54 @@
+
+
+pub mod log{
+
+    pub const C_INFO: u8 = 0b00000001;
+    pub const C_TEST: u8 = 0b00000010;
+    pub const C_WARN: u8 = 0b00000100;
+
+    // pub static mut CHECK: u8 = INFO | TEST | WARN;
+    pub static mut CHECK: u8 = C_INFO | C_TEST | C_WARN;
+
+    macro_rules! INFO {
+        ($($arg:tt)*) => (
+            #[allow(unused_unsafe)]
+            unsafe{
+                use crate::log::{CHECK, C_INFO};
+                if &CHECK & C_INFO == C_INFO {
+                    println!("[INFO] {}", std::fmt::format(format_args!($($arg)*)));
+                }
+            }
+        )
+    }
+    macro_rules! TEST {
+        ($($arg:tt)*) => (
+            #[allow(unused_unsafe)]
+            unsafe{
+                use crate::log::{CHECK, C_TEST};
+                if &CHECK & C_TEST == C_TEST {
+                    println!("[TEST] {}", std::fmt::format(format_args!($($arg)*)));
+                }
+            }
+        )
+    }
+    #[allow(unused_macros)]
+    macro_rules! WARN {
+        ($($arg:tt)*) => (
+            #[allow(unused_unsafe)]
+            unsafe{
+                use crate::log::{CHECK, C_WARN};
+                if &CHECK & C_WARN == C_WARN {
+                        eprintln!("[WARN] {}", std::fmt::format(format_args!($($arg)*)));
+                }
+
+            }
+        )
+    }
+
+
+
+    pub(crate) use INFO;
+    pub(crate) use TEST;
+    pub(crate) use WARN;
+
+}
