30aaf2092bf439d55b9e98a97aa20f714a4c37c3
JikoUnderscore <59426055+JikoUnderscore@users.noreply.github.com>
2021-12-13 15:15:56 +0200

    update

--- LOGGER.rs
+++ LOGGER.rs
@@ -1,27 +1,26 @@
+pub const C_INFO: u8 = 0b00000001;
+pub const C_TEST: u8 = 0b00000010;
+pub const C_WARN: u8 = 0b00000100;
 
+// contorl what gets printed form here
+pub static CHECK: u8 = C_INFO | C_TEST | C_WARN;
 
 pub mod log{
-
-    pub const C_INFO: u8 = 0b00000001;
-    pub const C_TEST: u8 = 0b00000010;
-    pub const C_WARN: u8 = 0b00000100;
-
-    pub static CHECK: u8 = C_INFO | C_TEST | C_WARN;
-
     macro_rules! INFO {
         ($($arg:tt)*) => (
             {
-                use crate::log::{CHECK, C_INFO};
+                use crate::LOGGER::{CHECK, C_INFO};
                 if &CHECK & C_INFO == C_INFO {
                     println!("[INFO] {}", std::fmt::format(format_args!($($arg)*)));
                 }
             }
         )
     }
+    
     macro_rules! TEST {
         ($($arg:tt)*) => (
             {
-                use crate::log::{CHECK, C_TEST};
+                use crate::LOGGER::{CHECK, C_TEST};
                 if &CHECK & C_TEST == C_TEST {
                     println!("[TEST] {}", std::fmt::format(format_args!($($arg)*)));
                 }
@@ -32,7 +31,7 @@ pub mod log{
     macro_rules! WARN {
         ($($arg:tt)*) => (
             {
-                use crate::log::{CHECK, C_WARN};
+                use crate::LOGGER::{CHECK, C_WARN};
                 if &CHECK & C_WARN == C_WARN {
                         eprintln!("[WARN] {}", std::fmt::format(format_args!($($arg)*)));
                 }
@@ -43,8 +42,9 @@ pub mod log{
 
 
 
+
+
     pub(crate) use INFO;
     pub(crate) use TEST;
     pub(crate) use WARN;
-
 }
